08cba510defc8a0d12c09feaf837493bcb952d3b
natimercy <qian.he@topevery.com>
2024-07-04 11:16:10 +0800

    feat: add spring-boot-shardingjdbc

---
+++ spring-boot-shardingjdbc/src/main/java/org/example/ShardingJdbcSpringBootApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ${NAME}
+ *
+ * @author <a href="mailto:natimercy93@gmail.com">natimercy</a>
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class ShardingJdbcSpringBootApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ spring-boot-shardingjdbc/src/test/java/org/example/ShardingJdbcSpringBootApplicationTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * ${NAME}
+ *
+ * @author <a href="mailto:natimercy93@gmail.com">natimercy</a>
+ * @version 1.0.0
+ */
+@SpringBootTest
+public class ShardingJdbcSpringBootApplicationTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello world!");
+
+    }
+
+}
